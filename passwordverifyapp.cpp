6015855d4f3e3fb1bbbc9d67ecbe655478cd9c76
Simanga Hendry <simangahk@gmail.com>
2019-09-04 05:54:08 +0200

    functionality complete;

--- passwordverifyapp.cpp
+++ passwordverifyapp.cpp
@@ -1,5 +1,6 @@
 #include "passwordverifyapp.h"
 #include "ui_passwordverifyapp.h"
+#include <QDebug>
 //#include "verify.h"
 
 PasswordVerifyApp::PasswordVerifyApp(QWidget *parent) :
@@ -8,26 +9,93 @@ PasswordVerifyApp::PasswordVerifyApp(QWidget *parent) :
     ui->setupUi(this);
 
     ui->matchLabel->hide();
-    ui->continueBtn->hide();
-
+    ui->continueBtn->hide();  
 
 }
 
-PasswordVerifyApp::~PasswordVerifyApp()
-{
+PasswordVerifyApp::~PasswordVerifyApp() {
     delete ui;
 }
 
 void PasswordVerifyApp::on_PasswordLineEdit_textChanged(const QString &arg1){
-    // isLong(arg1) ? mark() :
+    isLong(arg1) ? mark(ui->condition1) : unMark(ui->condition1);
+    checkNumber(arg1) ? mark(ui->condition2) : unMark(ui->condition2);
+    upperCheck(arg1) ? mark(ui->condition3) : unMark(ui->condition3);
+    lowerCheck(arg1) ? mark(ui->condition4) : unMark(ui->condition4);
+    specialCheck(arg1) ? mark(ui->condition5) : unMark(ui->condition5);
+}
+void PasswordVerifyApp::on_confirmPasswordLineEdit_textChanged(const QString &arg1){
+    //qDebug() << arg1;
+    if (ui->PasswordLineEdit->text() == arg1) {
+        ui->continueBtn->show();
+        ui->matchLabel->hide();
+    } else {
+        ui->continueBtn->hide();
+        ui->matchLabel->show();
+    }
+
+    if (arg1 == "") { ui->continueBtn->hide(); ui->matchLabel->hide();}
+
 }
 
-void PasswordVerifyApp::mark(const QCheckBox &condition){
-    if (!condition.isChecked()) {
-        ui->condition1->click();
-        ui->condition1->setStyleSheet("color: green");
+void PasswordVerifyApp::mark(QCheckBox *condition){
+    if (!condition->isChecked()) {
+        condition->click();
+        condition->setStyleSheet("color : green");
+        }
+}
+
+void PasswordVerifyApp::unMark(QCheckBox *condition){
+    if (condition->isChecked()) {
+       condition->click();
+       condition->setStyleSheet("color : white");
     }
 }
 
+bool PasswordVerifyApp::checkNumber(const QString str){
+    for (int i = 0; i < 10; i++) if (str.contains(QString::number(i))) return true;
+    return false;
+}
+
+bool PasswordVerifyApp::lowerCheck(const QString str){
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+       if (str[i].isLower()) count++;
+       if (count >= 3) return true;
+    }
+    return false;
+}
+
+bool PasswordVerifyApp::upperCheck(const QString str){
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+       if (str[i].isUpper()) count++;
+       if (count >= 3) return true;
+    }
+    return false;
+}
+
+bool PasswordVerifyApp::specialCheck(const QString str){
+    for (int i = 0; i < str.length(); i++) {
+       if (str.contains("!")) return true;
+       if (str.contains("@")) return true;
+       if (str.contains("&")) return true;
+       if (str.contains("#")) return true;
+       if (str.contains("*")) return true;
+       if (str.contains("+")) return true;
+       if (str.contains("-")) return true;
+       if (str.contains("~")) return true;
+       if (str.contains("%")) return true;
+       if (str.contains("$")) return true;
+       if (str.contains("\'")) return true;
+       if (str.contains(";")) return true;
+       if (str.contains("?")) return true;
+       if (str.contains("<")) return true;
+       if (str.contains(">")) return true;
+       if (str.contains(",")) return true;
+    }
+
+return false;
+}
 
 
--- passwordverifyapp.h
+++ passwordverifyapp.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QString>
+#include <QCheckBox>
 
 namespace Ui {
 class PasswordVerifyApp;
@@ -18,8 +19,14 @@ public:
 private slots:
     void on_PasswordLineEdit_textChanged(const QString &arg1);
     bool isLong(const QString &password) { return (password.length() >= 8 ? true : false); }
-    void mark();
-    void unMark();
+    void mark(QCheckBox *condition);
+    void unMark(QCheckBox *condition);
+    bool checkNumber(const  QString str);
+    bool lowerCheck(const  QString str);
+    bool upperCheck(const  QString str);
+    bool specialCheck(const  QString str);
+
+    void on_confirmPasswordLineEdit_textChanged(const QString &arg1);
 
 private:
     Ui::PasswordVerifyApp *ui;
